f89aebaa3fd85fcebfa9e47fd7e185d6909fe3f3
Nishanth Raj <nishanth.raj@abi-health.com>
2026-02-24 15:44:52 +0530

    made changes.. in app.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -5,35 +5,36 @@ exports.protect = async (req, res, next) => {
   try {
     let token;
 
-    // 1️⃣ Check cookie first
     if (req.cookies.token) {
       token = req.cookies.token;
     }
 
-    // 2️⃣ Or check Authorization header
-    else if (
-      req.headers.authorization &&
-      req.headers.authorization.startsWith("Bearer")
-    ) {
-      token = req.headers.authorization.split(" ")[1];
-    }
-
     if (!token) {
+      console.log("❌ No token found");
       return res.status(401).json({ message: "Not authorized" });
     }
 
+    console.log("✅ Token received:", token);
+    console.log("🔐 JWT_SECRET:", process.env.JWT_SECRET);
+
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    console.log("✅ Decoded ID:", decoded.id);
 
     const user = await User.findById(decoded.id).select("-password");
 
     if (!user) {
+      console.log("❌ User not found in DB");
       return res.status(401).json({ message: "User not found" });
     }
+
+    console.log("✅ User found:", user._id);
 
     req.user = user;
     next();
 
   } catch (error) {
-    res.status(401).json({ message: "Invalid or expired token" });
+    console.log("❌ JWT ERROR:", error.message);
+    return res.status(401).json({ message: error.message });
   }
 };
